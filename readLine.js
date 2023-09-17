c9b7e35275647d439a1a983902b39faf62b2f3d2
prajul-L850 <69051313+prajul-L850@users.noreply.github.com>
2023-09-17 10:35:37 +0530

    Add files via upload

---
+++ readLine.js
@@ -0,0 +1,29 @@
+const readline=require('readline');
+const r1=readline.createInterface({input:process.stdin,
+                          output:process.stdout});
+let num1=Math.floor((Math.random()*10+1));
+let num2=Math.floor((Math.random()*10+1));
+let answer=num1+num2;
+
+r1.question(`What is ${num1} + ${num2}? \n`,
+(userInput)=>{
+    if(userInput.trim() == answer){
+        r1.close();        
+    }
+    else{
+        r1.setPrompt('Incorrect response please try again \n');
+        r1.prompt();
+        r1.on('line',(userInput)=>{
+            if(userInput.trim() == answer)
+                r1.close();
+            else{
+                r1.setPrompt(`Your answer of ${userInput} is incorrect....Try again`);
+                r1.prompt();
+            }
+        })
+    }
+});
+
+r1.on('close',()=>{
+    console.log('Correct!!');
+});
